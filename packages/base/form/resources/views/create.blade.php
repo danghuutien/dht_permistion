ce6d625130621686601aecd96fbc2f436b98c79e
Đặng Hữu Tiến <danghuutientk123@gmail.com>
2024-03-24 22:19:58 +0700

    add editor

--- packages/base/form/resources/views/create.blade.php
+++ packages/base/form/resources/views/create.blade.php
@@ -4,5 +4,219 @@
     <label>Content</label>
     <textarea name="content" id="content"></textarea>
 </div> 
-
+<script src="https://cdn.ckeditor.com/ckeditor5/41.2.1/super-build/ckeditor.js"></script>
+<script>
+    // This sample still does not showcase all CKEditor&nbsp;5 features (!)
+    // Visit https://ckeditor.com/docs/ckeditor5/latest/features/index.html to browse all the features.
+    CKEDITOR.ClassicEditor.create(document.getElementById("content"), {
+        fontSize: {
+            options: [
+                9,
+                11,
+                13,
+                'default',
+                17,
+                16,
+                18,
+                19,
+                21,
+                22,
+                23,
+                24,
+                25,
+                26,
+                27,
+                28,
+                29,
+                30,
+                32,
+                34,
+                36,
+                38,
+                40
+            ],
+            supportAllValues: true
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html#extended-toolbar-configuration-format
+        toolbar: {
+            items: [
+                    'heading',
+                    '|',
+                    'fontColor',
+                    'fontSize',
+                    'fontBackgroundColor',
+                    'fontFamily',
+                    'bold',
+                    'italic',
+                    'underline',
+                    'link',
+                    'strikethrough',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'alignment',
+                    'direction',
+                    'outdent',
+                    'indent',
+                    '|',
+                    'uploadImage',
+                    'htmlEmbed',
+                    'blockQuote',
+                    'insertTable',
+                    'mediaEmbed',
+                    'undo',
+                    'redo',
+                    'findAndReplace',
+                    'removeFormat',
+                    'sourceEditing',
+                    'codeBlock',
+                ]
+        },
+        
+        // Changing the language of the interface requires loading the language file using the <script> tag.
+        language: 'vi',
+        list: {
+            properties: {
+                styles: true,
+                startIndex: true,
+                reversed: true
+            }
+        },
+        codeBlock: {
+            languages: [
+                {language: 'plaintext', label: 'Plain text'},
+                {language: 'c', label: 'C'},
+                {language: 'cs', label: 'C#'},
+                {language: 'cpp', label: 'C++'},
+                {language: 'css', label: 'CSS'},
+                {language: 'diff', label: 'Diff'},
+                {language: 'html', label: 'HTML'},
+                {language: 'java', label: 'Java'},
+                {language: 'javascript', label: 'JavaScript'},
+                {language: 'php', label: 'PHP'},
+                {language: 'python', label: 'Python'},
+                {language: 'ruby', label: 'Ruby'},
+                {language: 'typescript', label: 'TypeScript'},
+                {language: 'xml', label: 'XML'},
+                {language: 'dart', label: 'Dart', class: 'language-dart'},
+            ]
+        },
+        heading: {
+            options: [
+                {model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph'},
+                {model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1'},
+                {model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2'},
+                {model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3'},
+                {model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4'},
+                {model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5'},
+                {model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6'}
+            ]
+        },
+        placeholder: 'Chào mừng đến với website của đặng hữu tiến',
+        fontFamily: {
+            options: [
+                'default',
+                'Arial, Helvetica, sans-serif',
+                'Courier New, Courier, monospace',
+                'Georgia, serif',
+                'Lucida Sans Unicode, Lucida Grande, sans-serif',
+                'Tahoma, Geneva, sans-serif',
+                'Times New Roman, Times, serif',
+                'Trebuchet MS, Helvetica, sans-serif',
+                'Verdana, Geneva, sans-serif'
+            ],
+            supportAllValues: true
+        },
+        // Be careful with the setting below. It instructs CKEditor to accept ALL HTML markup.
+        // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html#enabling-all-html-features
+        htmlSupport: {
+            allow: [
+                {
+                    name: /.*/,
+                    attributes: true,
+                    classes: true,
+                    styles: true
+                }
+            ]
+        },
+        // Be careful with enabling previews
+        // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
+        htmlEmbed: {
+            showPreviews: true
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+        link: {
+            defaultProtocol: 'http://',
+            decorators: {
+                openInNewTab: {
+                    mode: 'manual',
+                    label: 'Open in a new tab',
+                    attributes: {
+                        target: '_blank',
+                    }
+                },
+                noFollow: {
+                    mode: "manual",
+                    label: "Nofollow",
+                    attributes: {
+                        rel: "nofollow",
+                    },
+                },
+            }
+        },
+        // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
+        mention: {
+            feeds: [
+                {
+                    marker: '@',
+                    feed: [
+                        '@apple', '@bears', '@brownie', '@cake', '@cake', '@candy', '@canes', '@chocolate', '@cookie', '@cotton', '@cream',
+                        '@cupcake', '@danish', '@donut', '@dragée', '@fruitcake', '@gingerbread', '@gummi', '@ice', '@jelly-o',
+                        '@liquorice', '@macaroon', '@marzipan', '@oat', '@pie', '@plum', '@pudding', '@sesame', '@snaps', '@soufflé',
+                        '@sugar', '@sweet', '@topping', '@wafer'
+                    ],
+                    minimumCharacters: 1
+                }
+            ]
+        },
+        // The "superbuild" contains more premium features that require additional configuration, disable them below.
+        // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
+        removePlugins: [
+            // These two are commercial, but you can try them out without registering to a trial.
+            // 'ExportPdf',
+            // 'ExportWord',
+            'AIAssistant',
+            'CKBox',
+            'CKFinder',
+            'EasyImage',
+            // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
+            // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
+            // Storing images as Base64 is usually a very bad idea.
+            // Replace it on production website with other solutions:
+            // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
+            // 'Base64UploadAdapter',
+            'RealTimeCollaborativeComments',
+            'RealTimeCollaborativeTrackChanges',
+            'RealTimeCollaborativeRevisionHistory',
+            'PresenceList',
+            'Comments',
+            'TrackChanges',
+            'TrackChangesData',
+            'RevisionHistory',
+            'Pagination',
+            'WProofreader',
+            // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
+            // from a local file system (file://) - load this site via HTTP server if you enable MathType.
+            'MathType',
+            // The following features are part of the Productivity Pack and require additional license.
+            'SlashCommand',
+            'Template',
+            'DocumentOutline',
+            'FormatPainter',
+            'TableOfContents',
+            'PasteFromOfficeEnhanced',
+            'CaseChange'
+        ]
+    });
+</script>
 @endsection
